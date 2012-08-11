a4a2bc7d2c933effdb3b6a78b72247958fd18719
Christopher Luebbemeier <christopher.luebbemeier@uni-weimar.de>
2012-08-11 03:19:42 +0200

    fix for xinitrc, cleanup of autostart

--- config/awesome/autostart.lua
+++ config/awesome/autostart.lua
@@ -8,18 +8,10 @@ function run_once(cmd)
 end
 
 
---awful.util.spawn_with_shell("sleep 5 && xsetroot -cursor_name left_ptr")
-awful.util.spawn_with_shell("xset r rate 250 30")
--- "/etc/X11/xorg.conf.d/10-synaptics.conf"
---awful.util.spawn_with_shell("synclient TouchpadOff=1")
---awful.util.spawn_with_shell("synclient HorizTwoFingerScroll=1")
-
-awful.util.spawn_with_shell("xmodmap $HOME/.Xmodmap")
-
+awful.util.spawn_with_shell("synclient TouchpadOff=1")
 
 run_once("pidgin")
--- run_once("urxvt -T irssi -pe -tabbed -e irssi")
-run_once("dropboxd")
-run_once("syndaemon -t -k -i 1 -d")
---run_once("nm-applet")
+--run_once("dropboxd")
+--run_once("syndaemon -t -k -i 1 -d")
+
 -- vim: ts=4 sw=4 et:
