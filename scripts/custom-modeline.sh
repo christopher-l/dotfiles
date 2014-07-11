7ebc9ebb6451b10096e090fada188395a16b6f52
Christopher Luebbemeier <christopher.luebbemeier@uni-weimar.de>
2014-07-11 11:34:04 +0200

    updated custom-modeline.sh

--- scripts/custom-modeline.sh
+++ scripts/custom-modeline.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # example call:
 # custom-modeline.sh HDMI1 1920 1080 60
 
-modeline=`cvt $2 $3 $4 | tail -n1 | cut -d " " -f 2-`
-modename=`cvt $2 $3 $4 | tail -n1 | cut -d " " -f 2`
+modeline=$(cvt $2 $3 $4 | tail -n1 | cut -d " " -f 2-)
+modename=$(cvt $2 $3 $4 | tail -n1 | cut -d " " -f 2)
 
-xrandr --newmode $modeline;
-xrandr --addmode $1 $modename;
-xrandr --output $1 --mode $modename --right-of LVDS1;
+xrandr --newmode $modeline
+xrandr --addmode $1 $modename
+xrandr --output $1 --mode $modename --right-of LVDS1
 
 nitrogen --restore
