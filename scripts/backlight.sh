199c300750d16314726155d63d2c449632f2f2c1
Christopher Luebbemeier <christopher.luebbemeier@uni-weimar.de>
2015-08-27 11:21:15 +0200

    general config update

--- scripts/backlight.sh
+++ scripts/backlight.sh
@@ -5,9 +5,11 @@
 #   backlight.sh 2
 #   backlight.sh 0.5
 
-echo foo
-new_value=$(echo "$(xbacklight -get) * (${1}) +1" | bc)
-echo bar
-xbacklight -set ${new_value} -time 100
-echo baz
+max_brightness=4437
+brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
+# rel_brightness=$(echo "100.0 * ${brightness}.0 / ${max_brightness}.0" | bc)
+new_value=$(echo "($brightness * (${1}) * 100 / $max_brightness) +1" | bc)
+echo $new_value
+xbacklight -set ${new_value} -steps 1
+# xbacklight -set ${new_value} -time 20 -steps 5
 # killall -SIGUSR1 i3status
