a7b27ddd9d02913ae4a3f96708b4906352791438
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-09-25 11:13:00 +0200

    update switch-theme

--- local/bin/switch-theme.sh
+++ local/bin/switch-theme.sh
@@ -2,11 +2,34 @@
 
 set -e
 
+function apply_vscode {
+    local config_file="$HOME/.config/Code - OSS/User/settings.json"
+    local dark_theme="Visual Studio Dark"
+    local light_theme="Visual Studio Light"
+    local chosen_theme=$1_theme
+    local theme=${!chosen_theme}
+    sed -i "s/\"workbench.colorTheme\": \".*\"/\"workbench.colorTheme\": \"$theme\"/" "$config_file"
+}
+
+# function apply_discord {
+#     local config_file="$HOME/.var/app/com.discordapp.Discord/config/discord/settings.json"
+#     local dark_theme="#202225"
+#     local light_theme="#ffffff"
+#     local chosen_theme=$1_theme
+#     local theme=${!chosen_theme}
+#     sed -i "s/\"BACKGROUND_COLOR\": \".*\"/\"BACKGROUND_COLOR\": \"$theme\"/" "$config_file"
+# }
+
 case "$1" in
-    light)
-        sed -i 's/"workbench.colorTheme": "Visual Studio Dark"/"workbench.colorTheme": "Visual Studio Light"/' "$HOME/.config/Code - OSS/User/settings.json"
+    light|dark)
+        apply_vscode $1
+        # apply_discord $1
         ;;
-    dark)
-        sed -i 's/"workbench.colorTheme": "Visual Studio Light"/"workbench.colorTheme": "Visual Studio Dark"/' "$HOME/.config/Code - OSS/User/settings.json"
+    *)
+        echo "Usage:"
+        echo "    $0 light"
+        echo "or"
+        echo "    $0 dark"
+        exit 1
         ;;
 esac
