9d584218050c1d6aa43f1b5f0298dbdaf63e65b6
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-11-07 10:48:57 +0100

    gnome-terminal support in switch-theme.sh

--- local/bin/switch-theme.sh
+++ local/bin/switch-theme.sh
@@ -2,34 +2,33 @@
 
 set -e
 
-function apply_vscode {
+function apply_vscode() {
     local config_file="$HOME/.config/Code - OSS/User/settings.json"
-    local dark_theme="Visual Studio Dark"
-    local light_theme="Visual Studio Light"
-    local chosen_theme=$1_theme
-    local theme=${!chosen_theme}
+    local dark_theme="Nord"
+    local light_theme="GitHub Light"
+    local selected_theme=$1_theme
+    local theme=${!selected_theme}
     sed -i "s/\"workbench.colorTheme\": \".*\"/\"workbench.colorTheme\": \"$theme\"/" "$config_file"
 }
 
-# function apply_discord {
-#     local config_file="$HOME/.var/app/com.discordapp.Discord/config/discord/settings.json"
-#     local dark_theme="#202225"
-#     local light_theme="#ffffff"
-#     local chosen_theme=$1_theme
-#     local theme=${!chosen_theme}
-#     sed -i "s/\"BACKGROUND_COLOR\": \".*\"/\"BACKGROUND_COLOR\": \"$theme\"/" "$config_file"
-# }
+function apply_gnome_terminal() {
+    local light_profile="b1dcc9dd-5262-4d8d-a863-c897e6d979b9"
+    local dark_profile="20933993-f2c5-4352-8629-fd26d2c438c6"
+    local selected_profile=$1_profile
+    local profile=${!selected_profile}
+    gsettings set org.gnome.Terminal.ProfilesList default $profile
+}
 
 case "$1" in
-    light|dark)
-        apply_vscode $1
-        # apply_discord $1
-        ;;
-    *)
-        echo "Usage:"
-        echo "    $0 light"
-        echo "or"
-        echo "    $0 dark"
-        exit 1
-        ;;
+light | dark)
+    apply_vscode $1
+    apply_gnome_terminal $1
+    ;;
+*)
+    echo "Usage:"
+    echo "    $0 light"
+    echo "or"
+    echo "    $0 dark"
+    exit 1
+    ;;
 esac
