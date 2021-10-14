dbfd70ef9c3623179650bec4457b786aea7ce03b
Christopher Lübbemeier <luebbemeier@metaventis.com>
2021-10-14 13:41:27 +0200

    add obisdian config

--- local/bin/switch-theme.sh
+++ local/bin/switch-theme.sh
@@ -23,6 +23,19 @@ function apply_vscode() {
     esac
 }
 
+function apply_obsidian() {
+    local workspaces=("$HOME/Documents/Zettelkasten")
+    for i in ${!workspaces[@]}; do
+        local config_file="${workspaces[$i]}/.obsidian/appearance.json"
+        local dark_theme="obsidian"
+        local light_theme="moonstone"
+        local selected_theme=$1_theme
+        local theme=${!selected_theme}
+        sed -i "s/\"theme\": \".*\"/\"theme\": \"$theme\"/" "$config_file"
+    done
+
+}
+
 function apply_gnome_terminal() {
     local light_profile="b1dcc9dd-5262-4d8d-a863-c897e6d979b9"
     local dark_profile="20933993-f2c5-4352-8629-fd26d2c438c6"
@@ -43,6 +56,7 @@ function apply_qt5() {
 case "$1" in
 light | dark)
     apply_vscode $1
+    apply_obsidian $1
     apply_qt5 $1
     # apply_gnome_terminal $1
     ;;
