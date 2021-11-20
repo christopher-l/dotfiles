a122d4ea830335b4f624c12c76a37ddef8f754e2
Christopher Lübbemeier <luebbemeier@metaventis.com>
2021-11-20 13:18:32 +0100

    minor update

--- local/bin/switch-theme.sh
+++ local/bin/switch-theme.sh
@@ -16,12 +16,12 @@ function apply_vscode() {
     case "$1" in
     light)
         sed -i "s/\"workbench.colorTheme\": \"\(.*\)Dark\(.*\)\"/\"workbench.colorTheme\": \"\1Light\2\"/" "$config_file"
-        sed -i "s/\"editor.fontFamily\": \"\(.*\) Medium\"/\"editor.fontFamily\": \"\1\"/" "$config_file"
+        # sed -i "s/\"editor.fontFamily\": \"\(.*\) Medium\"/\"editor.fontFamily\": \"\1\"/" "$config_file"
         ;;
     dark)
         sed -i "s/\"workbench.colorTheme\": \"\(.*\)Light\(.*\)\"/\"workbench.colorTheme\": \"\1Dark\2\"/" "$config_file"
         # Append "Medium" to the "fontFamily" value, but only if it currently doesn't have "Medium" as suffix.
-        sed -i -E '/\"editor\.fontFamily\": \"(.*)Medium\"/! s/\"editor\.fontFamily\": \"(.*)\"/\"editor\.fontFamily\": \"\1 Medium\"/' "$config_file"
+        # sed -i -E '/\"editor\.fontFamily\": \"(.*)Medium\"/! s/\"editor\.fontFamily\": \"(.*)\"/\"editor\.fontFamily\": \"\1 Medium\"/' "$config_file"
         ;;
     esac
 }
