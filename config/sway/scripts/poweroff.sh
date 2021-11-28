330494ce5c4b4e93041c71cdff649926decb76d2
Christopher Lübbemeier <luebbemeier@metaventis.com>
2021-11-28 13:25:23 +0100

    sway updates

--- config/sway/scripts/poweroff.sh
+++ config/sway/scripts/poweroff.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env bash
 
-yad \
-    --title="Power off system" \
-    --button="Cancel" \
-    --button="Reboot" \
-    --button="Power off" \
+function all() (
+    yad \
+        --title="Exit session" \
+        --button="Cancel" \
+        --button="Log out" \
+        --button="Restart" \
+        --button="Power off"
 
-case $? in
-   1) reboot;;
-   2) poweroff;;
+    case $? in
+    1) swaymsg exit ;;
+    2) reboot ;;
+    3) poweroff ;;
+    esac
+)
+
+function poweroff() (
+    yad \
+        --title="Power off system" \
+        --button="Cancel" \
+        --button="Power off"
+
+    case $? in
+    1) poweroff ;;
+    esac
+)
+
+case "$1" in
+all) all ;;
+poweroff) poweroff ;;
 esac
