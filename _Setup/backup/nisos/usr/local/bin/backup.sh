0aa52b34145999ce5ada20b1ae9aa4c83bed4825
Christopher Lübbemeier <luebbemeier@metaventis.com>
2023-02-21 19:45:05 +0100

    update backup script

--- _Setup/backup/nisos/usr/local/bin/backup.sh
+++ _Setup/backup/nisos/usr/local/bin/backup.sh
@@ -2,37 +2,67 @@
 
 set -e
 
-DEST=root@torus:/mnt/hdd/backup/weekly.0/nisos
-# DEST=/mnt/wd-raid/Backup/nisos
-
-args=(
-    -a
-    # -P
-    # --info=progress2
-    -e ssh
-    --exclude=/dev
-    --exclude=/sys
-    --exclude=/proc
-    --exclude=/run
-    # --exclude=/mnt
-    --exclude=/tmp
-    --exclude=/media
-    --exclude=/var/cache/
-    --exclude=/var/lib/docker
-    --exclude=.cache
-    --exclude=.local/share/Trash
-    --exclude=.m2/repository
-    --exclude=.npm
-    --exclude=.var/app/com.valvesoftware.Steam
-    --exclude=node_modules
-    --exclude=.angular
-    --exclude=".Trash-*"
-    --exclude=Backup
-    --exclude=/mnt/wd-raid/Incoming
-    --exclude=/mnt/wd-raid/Video
-    --delete
-    --delete-excluded
+function print_usage_and_exit() (
+    echo "Usage: $0 (online|cold)"
+    exit 1
 )
 
-echo rsync / "${args[@]}" $DEST/
-rsync / "${args[@]}" $DEST/
+if [ "$#" -ne 1 ]; then
+    print_usage_and_exit
+fi
+
+preset="$1"
+
+function main() (
+    args=(
+        -a
+        --exclude=/dev
+        --exclude=/sys
+        --exclude=/proc
+        --exclude=/run
+        # --exclude=/mnt
+        --exclude=/tmp
+        --exclude=/media
+        --exclude=/var/cache/
+        --exclude=/var/lib/docker
+        --exclude=.cache
+        --exclude=.local/share/Trash
+        --exclude=.m2/repository
+        --exclude=.npm
+        --exclude=.var/app/com.valvesoftware.Steam
+        --exclude=node_modules
+        --exclude=.angular
+        --exclude=".Trash-*"
+        --exclude=/mnt/wd-raid/Incoming
+        --exclude=/mnt/wd-raid/Video
+        --delete
+        --delete-excluded
+    )
+
+    case "$preset" in
+    online)
+        dest=root@torus:/mnt/hdd/backup/weekly.0/nisos
+        args+=(
+            -e ssh
+        )
+        ;;
+    cold)
+        dest=/run/media/chris/backup/nisos
+        args+=(
+            -P
+            --info=progress2
+            --exclude=/mnt/wd-raid/Backup
+            --exclude=/mnt/wd-raid/Music
+            --exclude=/mnt/wd-raid/VMs
+        )
+        ;;
+    *)
+        print_usage_and_exit
+        ;;
+    esac
+
+    echo rsync "${args[@]}" / $dest/
+    rsync "${args[@]}" / $dest/
+)
+
+main
