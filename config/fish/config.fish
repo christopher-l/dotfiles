298635a533ed132105b7c72b3cba5bacd2f078a9
Christopher Lübbemeier <luebbemeier@metaventis.com>
2023-02-25 20:02:05 +0100

    reset some fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,22 +1,4 @@
-function restore_theme
-    if type -q theme.sh; and test -e ~/.theme_history
-        theme.sh (theme.sh -l|tail -n1)
-    end
-end
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
-    # if test "$TERM_PROGRAM" != vscode
-    #     restore_theme
-    # end
-end
-
-function fish_title
-    if set -q argv[1]
-        echo $argv;
-    else
-        # pwd | truncate_pwd 26
-        basename (pwd)
-    end
 end
