e869c16c299b4a043e5e99f0c9b11aaf6b8baf93
Christopher Lübbemeier <luebbemeier@metaventis.com>
2023-07-22 20:42:03 +0200

    vscode updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,3 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
 end
+
+if string match -q "$TERM_PROGRAM" "vscode"
+     . (code --locate-shell-integration-path fish)
+end
