cb520f4d60278b0db25ee4847c761a4cb89dfdd9
Christopher Lübbemeier <luebbemeier@metaventis.com>
2023-08-07 10:55:36 +0200

    use qgnomeplatform

--- local/bin/switch-theme.sh
+++ local/bin/switch-theme.sh
@@ -137,11 +137,11 @@ function apply_icon_theme() (
 
 function apply_all_applications() (
     # apply_vscode $1
-    apply_obsidian $1
+    # apply_obsidian $1
     # apply_ulauncher $1
-    apply_qt5 $1
+    # apply_qt5 $1
     # apply_gnome_terminal $1
-    apply_chromium $1
+    # apply_chromium $1
 )
 
 
