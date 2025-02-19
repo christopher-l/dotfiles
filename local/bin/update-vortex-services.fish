aebdbe60fa8e638bf8ac411003b94f60a526d835
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2025-02-19 19:53:47 +0100

    Revert "Add nocodb"
    This reverts commit 7ccc44d4d92a0a57d39ec3d388e14ba1f2eb5612.

--- local/bin/update-vortex-services.fish
+++ local/bin/update-vortex-services.fish
@@ -3,7 +3,7 @@
 echo "Pruning docker..."
 ssh vortex docker system prune --all --volumes --force; or return
 
-set services miniflux hedgedoc paperless-ngx nocodb
+set services miniflux hedgedoc paperless-ngx
 
 for service in $services
     echo ""
