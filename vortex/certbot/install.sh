3a58abccbb043c70ce11b0ab3b37aa192d9581f1
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2025-09-13 02:34:13 +0200

    Replace nginx with caddy

--- vortex/certbot/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-systemctl daemon-reload
-systemctl enable certbot-renew.timer
-systemctl start certbot-renew.timer
--- vortex/nginx/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-nginx -t
-systemctl enable nginx
-systemctl restart nginx
