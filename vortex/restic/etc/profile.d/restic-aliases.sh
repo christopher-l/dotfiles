18a6a34993eda781e5667f4e19ef8d857c54090a
Christopher Lübbemeier <Christopher.Luebbemeier@la.thueringen.de>
2026-03-09 18:41:06 +0100

    Restic aliases

---
+++ vortex/restic/etc/profile.d/restic-aliases.sh
@@ -0,0 +1,2 @@
+alias restic-local='sudo -u restic /home/restic/bin/restic -r /mnt/hdd/backup -p /home/restic/restic-password-local'
+alias restic-gdrive='sudo -u restic /home/restic/bin/restic -r rclone:gdrive:Backup -p /home/restic/restic-password-gdrive'
