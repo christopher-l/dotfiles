c0dc38707e7ed961a3c2c45d25c13c43645ddcf2
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2026-03-20 22:33:07 +0100

    Delete restic-self-update.sh

--- vortex/restic/usr/local/bin/restic-self-update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-~restic/bin/restic self-update
-chown root:restic ~restic/bin/restic
-chmod 750 ~restic/bin/restic
-setcap cap_dac_read_search=+ep ~restic/bin/restic
